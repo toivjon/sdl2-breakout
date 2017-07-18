3e72010e69c0dcb7116704f2e101e51eab1d2291
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2017-07-18 21:07:45 +0300

    Modified the file description in the main.cpp to be more consistent with the documentation style used within the header files.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 /*! The entry point of the application.
-  
-  This is the entry point for an old and traditional Breakout game implemented
-  with the SDL2 library. This entry point contains a simple logic where we only
-  create, start and close the application as required. See the other files for
-  more interesting details how the actual game is implemented.
+ *  
+ * This is the entry point for an old and traditional Breakout game implemented
+ * with the SDL2 library. This entry point contains a simple logic where we only
+ * create, start and close the application as required. See the other files for
+ * more interesting details how the actual game is implemented.
  */
  #include "game.h"
  
