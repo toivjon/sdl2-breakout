d6ad41050b31e205efd418f5b487b52566af4389
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2017-07-18 20:44:09 +0300

    Added the missing file description into the paddle.h.

--- include/paddle.h
+++ include/paddle.h
@@ -1,3 +1,11 @@
+/*! A paddle implementation for the Breakout game.
+ *
+ *  This class encapsulates all the additional data required to manage a game
+ *  paddle that acts as a player avatar in the Breakout game. Paddle is itself
+ *  a simple rectangle component that is simply handled by the user by pressing
+ *  the movement buttons. In two player games, both players may share the same
+ *  avatar instance as the players are not playing the game at the same time.
+ */
 #ifndef BREAKOUT_PADDLE_H
 #define BREAKOUT_PADDLE_H
 
@@ -9,6 +17,8 @@ namespace breakout
   {
   public:
     Paddle(Game& game);
+
+    virtual ~Paddle() = default;
 
     void shrink();
     void reset();
